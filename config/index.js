3001be2a3f59d121a90b340be62f9016746c7d98
unknown <bleed-it-out@live.com>
2021-02-13 16:50:30 -0300

    add config folder

---
+++ config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  BLIZZARD: {
+    CLIENT_ID: process.env.BLIZZ_CLIENT_ID,
+    CLIENT_SECRET: process.env.BLIZZ_CLIENT_SECRET,
+    PROTOCOL: 'https',
+    URL: 'api.blizzard.com',
+    AUTH_URL: 'https://us.battle.net/oauth/token'
+  }
+}
