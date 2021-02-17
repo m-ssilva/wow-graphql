ecc3e016c6941923708826b27716806e853b7248
unknown <bleed-it-out@live.com>
2021-02-17 12:02:21 -0300

    remove unused integration from character resolver

--- src/resolvers/character/query.js
+++ src/resolvers/character/query.js
@@ -1,5 +1,3 @@
-const { blizzard } = require('../../integrations')
-
 const getCharacterInfo = async (_, { region, realm, character_name }, context, info) => ({
   Query: { region, realm, character_name },
   Context: { ...context },
