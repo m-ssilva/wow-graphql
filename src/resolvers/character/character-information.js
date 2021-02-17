57ba52d3adfac98c6f5f61d5f5420609ef6a37a6
unknown <bleed-it-out@live.com>
2021-02-17 12:03:56 -0300

    profile resolvers improvements

--- src/resolvers/character/character-information.js
+++ src/resolvers/character/character-information.js
@@ -1,88 +1,97 @@
 const { blizzard } = require('../../integrations')
+const builders = require('../../builders')
+const _ = require('lodash')
 
-const name = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo) return characterInfo.name
-}
+const profile = async ctx => {
+  const characterProfileInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
 
-const gender = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo) return characterInfo.gender.name
+  const getFaction = async () => {
+    if (characterProfileInfo && characterProfileInfo.faction) return characterProfileInfo.faction.name
+  }
+
+  const getRace = async () => {
+    if (characterProfileInfo && characterProfileInfo.race) return characterProfileInfo.race.name
+  }
+
+  const getCharacterClass = async () => {
+    if (characterProfileInfo && characterProfileInfo.character_class) return characterProfileInfo.character_class.name
+  }
+
+  const getSpecialization = async () => {
+    if (characterProfileInfo) return characterProfileInfo.active_spec.name
+  }
+
+  const getGuild = async () => {
+    if (characterProfileInfo.guild && characterProfileInfo.guild.name) return characterProfileInfo.guild.name
+  }
+
+  const getLevel = async () => characterProfileInfo.level
+
+  const getItemLevel = async () => characterProfileInfo.equipped_item_level
+
+  const getAchievementPoints = async () => characterProfileInfo.achievement_points
+
+  const getCovenant = async () => {
+    if (characterProfileInfo && characterProfileInfo.covenant_progress && characterProfileInfo.covenant_progress.chosen_covenant)
+      return characterProfileInfo.covenant_progress.chosen_covenant.name
+  }
+
+  const getCovenantRenown = async () => {
+    if (characterProfileInfo && characterProfileInfo.covenant_progress) return characterProfileInfo.covenant_progress.renown_level
+  }
+
+  return {
+    name: _.get(characterProfileInfo, 'name'),
+    gender: _.get(characterProfileInfo, 'gender.name'),
+    faction: getFaction(),
+    race: getRace(),
+    character_class: getCharacterClass(),
+    specialization: getSpecialization(),
+    guild: getGuild(),
+    level: getLevel(),
+    item_level: getItemLevel(),
+    achievement_points: getAchievementPoints(),
+    covenant: getCovenant(),
+    covenant_renown: getCovenantRenown()
+  }
 }
 
 const media = async ctx => {
   const characterMedia = await blizzard.character.characterImageLoader.load(ctx.Query)
-  if (characterMedia.assets) {
-    const characterMainImage = characterMedia.assets.find(asset => asset.key === 'main')
-    if (characterMainImage)
-      return { character_image_url: characterMainImage.value }
+
+  const getCharacterImageURL = async () => {
+    if (characterMedia.assets) {
+      const characterMainImage = characterMedia.assets.find(asset => asset.key === 'main')
+      if (characterMainImage)
+        return characterMainImage.value
+    }
+  }
+
+  return {
+    character_image_url: getCharacterImageURL()
   }
 }
 
-const faction = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo && characterInfo.faction) return characterInfo.faction.name
-}
+const arena = async ctx => {
+  const [duoBracket, trioBracket] = await Promise.allSettled([
+    blizzard.arena.arenaDuoBracketLoader.load(ctx.Query),
+    blizzard.arena.arenaTrioBracketLoader.load(ctx.Query)
+  ])
 
-const race = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo && characterInfo.race) return characterInfo.race.name
-}
+  const getBracket = async () => {
+    return [
+      builders.bracket.arenaBracket(duoBracket.value, 'Arena 2v2'),
+      builders.bracket.arenaBracket(trioBracket.value, 'Arena 3v3')
+    ].filter(bracket => bracket)
+  }
 
-const character_class = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo && characterInfo.character_class) return characterInfo.character_class.name
-}
-
-const specialization = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo)
-    return characterInfo.active_spec.name
-}
-
-const guild = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo.guild && characterInfo.guild.name) return characterInfo.guild.name
-}
-
-const level = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  return characterInfo.level
-}
-
-const item_level = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  return characterInfo.equipped_item_level
-}
-
-const achievement_points = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  return characterInfo.achievement_points
-}
-
-const covenant = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo && characterInfo.covenant_progress && characterInfo.covenant_progress.chosen_covenant)
-    return characterInfo.covenant_progress.chosen_covenant.name
-}
-
-const covenant_renown = async ctx => {
-  const characterInfo = await blizzard.character.characterInfoLoader.load(ctx.Query)
-  if (characterInfo && characterInfo.covenant_progress) return characterInfo.covenant_progress.renown_level
+  return {
+    bracket: getBracket()
+  }
 }
 
 module.exports = {
-  name,
-  gender,
-  faction,
-  race,
-  character_class,
-  specialization,
-  guild,
-  level,
-  item_level,
-  achievement_points,
-  covenant,
-  covenant_renown,
-  media
+  profile,
+  media,
+  arena
 }
