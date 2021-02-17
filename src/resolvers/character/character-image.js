72cb803317be85e092fceeeaec1f18bfdc101703
unknown <bleed-it-out@live.com>
2021-02-17 12:03:25 -0300

    remove unused resolver

--- src/resolvers/character/character-image.js
+++
@@ -1,8 +0,0 @@
-const character_image_url = body => {
-  if (body.assets) {
-    const characterImage = body.assets.find(asset => asset.key === 'main')
-    if (characterImage) return characterImage.value
-  }
-}
-
-module.exports = { character_image_url }
