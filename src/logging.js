4229147d547bf53c1da8aba6ad12abebac67fbaa
unknown <bleed-it-out@live.com>
2021-02-23 11:35:36 -0300

    log only on productive environments

--- src/logging.js
+++ src/logging.js
@@ -1,9 +1,11 @@
 const logging = {
   requestDidStart: (requestContext) => {
     if (requestContext.request.operationName !== 'IntrospectionQuery') {
-      console.log('Request started! Query')
-      console.log('Variables:', requestContext.request.variables)
-      console.log('Query:', requestContext.request.query)
+      if (!process.env.NODE_ENV === 'test') {
+        console.log('Request started! Query')
+        console.log('Variables:', requestContext.request.variables)
+        console.log('Query:', requestContext.request.query)
+      }
     }
   }
 }
